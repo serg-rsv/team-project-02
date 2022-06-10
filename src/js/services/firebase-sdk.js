2601292e344ea0b01bf7684c7c1096a5322e4dc4
Serhii Rybak <rybaksw@gmail.com>
2022-06-10 23:23:12 +0300

    інстальовано firebase lib and sdk
    також інстальовано:
    simplelightbox
    lodash

---
+++ src/js/services/firebase-sdk.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyAutR8LQAryGBD4KQz3S34HFCIHoEhmpOI',
+  authDomain: 'filmoteka-7196f.firebaseapp.com',
+  projectId: 'filmoteka-7196f',
+  storageBucket: 'filmoteka-7196f.appspot.com',
+  messagingSenderId: '345761584449',
+  appId: '1:345761584449:web:4d0491a5aa57059d43fdc0',
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
