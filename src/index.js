ba08cba84cf799c1462e6c0de13956a1772f6174
Serhii Rybak <rybaksw@gmail.com>
2022-06-12 21:32:17 +0300

    babel update

--- src/index.js
+++ src/index.js
@@ -3,4 +3,4 @@ import './sass/main.scss';
 import TmdbApiService from './js/services/tmdb-api';
 import { renderMainPage } from './js/Oleksandr/render';
 
-// renderMainPage(TmdbApiService.fetchTrendingMovies());
+renderMainPage(TmdbApiService.fetchTrendingMovies());
