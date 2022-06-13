a3cf3c85befd44551d4d174d8e060b02f5f0c83c
IvanSkliar <skliariv91@gmail.com>
2022-06-13 22:08:20 +0300

    1/2

--- src/index.js
+++ src/index.js
@@ -6,6 +6,6 @@ import './sass/main.scss';
 import { TmdbApiService } from './js/services/tmdb-api';
 import { renderMainPage } from './js/Oleksandr/render';
 import './js/irina/modal.js';
-
+import './js/Ivan/input-search';
 renderMainPage(TmdbApiService.fetchTrendingMovies());
 
