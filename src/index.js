e40a75f48951c18974c35a99f0c627367ce69f54
Nikolay Tymoshchuk <saintnikfreak@gmail.com>
2022-06-13 10:57:38 +0300

    buttons styles

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import './sass/main.scss';
 
 import { TmdbApiService } from './js/services/tmdb-api';
 import { renderMainPage } from './js/Oleksandr/render';
-import './js/irina/modal.js';
+import  './js/irina/modal.js';
 
 renderMainPage(TmdbApiService.fetchTrendingMovies());
