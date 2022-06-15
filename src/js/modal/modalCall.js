ef46e2f61a2db366de10a027d4c30a7cf4553c53
Serhii Rybak <88489822+serg-rsv@users.noreply.github.com>
2022-06-15 16:50:02 +0300

    Update modalCall.js

--- src/js/modal/modalCall.js
+++ src/js/modal/modalCall.js
@@ -4,23 +4,31 @@ import 'basiclightbox/src/styles/main.scss';
 
 /**
  * 
- * @param {*} data - Обязательный атрибут. Принимает данные, которые необходимо отобразить в модальном окне. Например функцию хендлбарса с разметкой
- * @param {string} selector - Необязательный атрибут. Принимает селектор, валидный для document.querySelector. Например '.form_close-button' или '[data-modal-close]'. Обязательно в кавычках. Это ссылка на кнопку закрытия модального окна, если она предусмотрена.
+ * @param {*} data - Обязательный атрибут. Принимает разметку, которую необходимо отобразить в модальном окне. 
+ * Например функцию хендлбарса с разметкой.
+ * @param {string} selector - Необязательный атрибут. Принимает селектор, валидный для document.querySelector. 
+ * Например '.form_close-button' или '[data-modal-close]'. Обязательно в кавычках. Это ссылка на кнопку закрытия 
+ * модального окна, если она предусмотрена.
  * @returns 
  */
 
 export function modalCall(data, selector) {
   const instance = basicLightbox.create(data);
-  console.log('selector :>> ', `'${selector}'`);
   instance.show();
-  document.body.addEventListener('keydown', (e) => {
-    if (e.key === 'Escape') {
-      instance.close();
-    }
-  })
+  document.body.addEventListener('keydown', onEscapeKeyDown);
+
   if (!selector) {
     return;
   }
+
   const newItem = document.querySelector(selector);
   newItem.addEventListener('click', () => instance.close());
+
+  function onEscapeKeyDown(e) {
+    if (e.key === 'Escape') {
+      instance.close();
+      document.body.removeEventListener('keydown', onEscapeKeyDown);
+    }
+  }
 }
+
