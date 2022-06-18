4d12c2edd6f3f4d851bcbbf90971cdf5992805ac
Kaplynenko <93135352+Kaplynenko@users.noreply.github.com>
2022-06-18 17:30:32 +0300

    fixed

--- src/js/kaplunenko/render.js
+++ src/js/kaplunenko/render.js
@@ -6,14 +6,25 @@ import { tmdbApi } from '../services/tmdb-api';
 
 const refFilmsList = document.querySelector('.films_list');
 
+function getShortGenresString(arrayGenres) {
+  if (arrayGenres.length <= 2) {
+    return arrayGenres.join(', ');
+  } else {
+    const shortGener = arrayGenres.slice(0, 2);
+    shortGener.push('Other');
+    return shortGener.join(', ');
+  }
+}
+
 export function renderMainPage(movies) {
   const descriptionMarkup = movies
     .map(({ id, title, genres, posterUrl, releaseYear, vote_average }) => {
+      const genresString = getShortGenresString(genres);
       return `<li class="products__cards-item" data-movie-id="${id}">
                 <img class="img" src="${posterUrl}" >
                 <p class="film_title">${title}</p>
                 <ul class="info-list">
-                    <li class="film_genre">${genres.join(', ')}</li>
+                    <li class="film_genre">${genresString}</li>
                     <li class="film_genre">${releaseYear}</li>
                     <li class="film-profile__value--accent film_average">${vote_average}</li>
                 </ul>
@@ -25,45 +36,6 @@ export function renderMainPage(movies) {
 }
 
 export function openDetailsCard(detailsCard, selectorCloseBtn) {
-  // refFilmsList.addEventListener('click', handleClick);
-  // function handleClick(event) {
-  //   const swatch = event.target;
-  //   const cardFilm = swatch.closest('.products__cards-item');
-  //   const getMovieId = cardFilm.dataset.movieId;
-  //   const movieIdstringToNumber = Number(getMovieId);
-  //   const filmId = movies.map(item => item.id);
-
-  //   if (filmId.includes(movieIdstringToNumber)) {
-  //     const detailsCard = movies.filter(item => item.id === movieIdstringToNumber);
   const markup = cardTpl(detailsCard);
   modalCall(markup, selectorCloseBtn);
-  // }
-  // }
-}
-
-// function genresListCut(array) {
-//   const genresList = array.map(({ genres }) => {
-//     if (genres.length === 2) {
-//       return genres;
-//     }
-//   });
-//   console.log(genresList);
-// }
-// function onFetchMovieSaveData(movieList, userId) {
-//   DATA_FROM_DATABASE = {
-//     userId,
-//     movieList,
-//   };
-// }
-// console.log(onFetchMovieSaveData());
-// const wrt = tmdbApi.fetchTrendingMovies();
-// console.log(
-//   wrt.then(value => {
-//     return value;
-//   }),
-// );
-tmdbApi.fetchTrendingMovies().then(rrr);
-
-function rrr(test) {
-  return test;
 }
