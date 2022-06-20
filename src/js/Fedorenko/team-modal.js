2527ab10e19837e0a6952b4bafdcc981b3d14f5f
Nikolay Tymoshchuk <saintnikfreak@gmail.com>
2022-06-20 14:48:33 +0300

    allmost finish team modal

--- src/js/Fedorenko/team-modal.js
+++ src/js/Fedorenko/team-modal.js
@@ -11,7 +11,7 @@ const data = [
   {
     id: 'serhii',
     name: 'Serhii Rybak',
-    position: 'Team lead',
+    position: 'team lead',
     imagesolar1xURL: 'https://i.ibb.co/xst44J2/sergiy-1x.png',
     imagesolar2xURL: 'https://i.ibb.co/dJBfH4W/sergiy-2x.png',
     imagecolorized1xURL: 'https://i.ibb.co/tqxd92y/sergiy-1x.png',
@@ -23,7 +23,7 @@ const data = [
   {
     id: 'mykola',
     name: 'Mykola Tymoshchuk',
-    position: 'Scrum master',
+    position: 'scrum master',
     imagesolar1xURL: 'https://i.ibb.co/pLq8RhN/mykola-1x.png',
     imagesolar2xURL: 'https://i.ibb.co/yfkLn1F/mykola-2x.png',
     imagecolorized1xURL: 'https://i.ibb.co/8jZdGG9/mykola-1x.png',
@@ -35,7 +35,7 @@ const data = [
   {
     id: 'svitlana',
     name: 'Svitlana Fedorenko',
-    position: 'Developer',
+    position: 'developer',
     imagesolar1xURL: 'https://i.ibb.co/b3V73BW/svetlana-1x.png',
     imagesolar2xURL: 'https://i.ibb.co/HqK719x/svetlana-2x.png',
     imagecolorized1xURL: 'https://i.ibb.co/94XfySt/svetlana-1x.png',
@@ -47,7 +47,7 @@ const data = [
   {
     id: 'dmytro',
     name: 'Dmytro Prokoptsov',
-    position: 'Developer',
+    position: 'developer',
     imagesolar1xURL: 'https://i.ibb.co/dQHnGkY/dmytro-1x.png',
     imagesolar2xURL: 'https://i.ibb.co/5TcNc29/dmytro-2x.png',
     imagecolorized1xURL: 'https://i.ibb.co/0mv7H0S/dmytro-1x.png',
@@ -59,7 +59,7 @@ const data = [
   {
     id: 'tetiana',
     name: 'Tetiana Mykhailenko',
-    position: 'Developer',
+    position: 'developer',
     imagesolar1xURL: 'https://i.ibb.co/dLX5vx6/tetyana-1x.png',
     imagesolar2xURL: 'https://i.ibb.co/mbX3H3k/tetyana-2x.png',
     imagecolorized1xURL: 'https://i.ibb.co/HXsW5c8/tetyana-1x.png',
@@ -71,7 +71,7 @@ const data = [
   {
     id: 'ivan',
     name: 'Ivan Skliar',
-    position: 'Developer',
+    position: 'developer',
     imagesolar1xURL: 'https://i.ibb.co/VY9x9GH/ivan-1x.png',
     imagesolar2xURL: 'https://i.ibb.co/3YVsxbz/ivan-2x.png',
     imagecolorized1xURL: 'https://i.ibb.co/CbgTNBW/ivan-1x.png',
@@ -83,7 +83,7 @@ const data = [
   {
     id: 'iryna',
     name: 'Iryna Honchar',
-    position: 'Developer',
+    position: 'developer',
     imagesolar1xURL: 'https://i.ibb.co/HGtzmv4/iryna-1x.png',
     imagesolar2xURL: 'https://i.ibb.co/TWxq9Km/iryna-2x.png',
     imagecolorized1xURL: 'https://i.ibb.co/J5H77xw/iryna-1x.png',
@@ -95,7 +95,7 @@ const data = [
   {
     id: 'oleksandr',
     name: 'Oleksandr Kaplunenko',
-    position: 'Developer',
+    position: 'developer',
     imagesolar1xURL: 'https://i.ibb.co/6XcfX6L/oleksandr-1x.png',
     imagesolar2xURL: 'https://i.ibb.co/fSxPBwK/oleksandr-2x.png',
     imagecolorized1xURL: 'https://i.ibb.co/sP3xBwX/oleksandr-1x.png',
@@ -107,13 +107,13 @@ const data = [
   {
     id: 'taras',
     name: 'Taras Bondar',
-    position: 'Developer',
+    position: 'developer',
     imagesolar1xURL: 'https://i.ibb.co/vPqXLRc/taras-1x.png',
     imagesolar2xURL: 'https://i.ibb.co/7vmrbQV/taras-2x.png',
     imagecolorized1xURL: 'https://i.ibb.co/whfzsTN/taras-1x.png',
     imagecolorized2xURL: 'https://i.ibb.co/VS27G5n/taras-2x.png',
     github: 'https://github.com/strelezzzz',
-    linkdIn: 'linkedin.com/in/taras-bondar-468131119',
+    linkdIn: 'https://www.linkedin.com/in/taras-bondar-468131119',
     mail: 'strelezzzz@gmai.com',
   },
 ];
